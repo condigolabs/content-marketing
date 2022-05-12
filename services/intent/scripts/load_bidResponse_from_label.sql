bb2eeb9c68aa81ec8883327cf2157e6f720ea963
srocchiccioli <Augmented!1>
2022-05-12 21:53:42 +0200

    Add Image Add BidRequests Add Lines Formating

--- services/intent/scripts/load_bidResponse_from_label.sql
+++ services/intent/scripts/load_bidResponse_from_label.sql
@@ -1,26 +1,26 @@
 WITH R AS ( 
    SELECT
-        DISTINCT RequestId ,L.ID
-   FROM `core-ssp.machinelearning.usa_products_unnested`
+        R.*,L.ID as LabelId, L.Label
+   FROM `core-ssp.machinelearning.usa_products_unnested` R
    INNER JOIN `core-ssp.machinelearning.GPT_Labels` L ON L.Label=  Products.Label
    AND L.ID = {{.LabelId}} AND Locale ="{{.Locale}}"
-   ORDER BY RAND() DESC  LIMIT 1
+   ORDER BY RAND() DESC  LIMIT 3
 )
-  SELECT 
-  R.RequestId
+  SELECT
+   MAX(RequestId) as RequestId
   ,MAX(Country)  as Country
- ,L.ID  as LabelId
- ,MAX(Date)  AS DATE
- ,"{{.Locale}}" as Locale
- ,MAX(L.Label) as Label
- ,ARRAY_AGG( STRUCT (
- Products.Title
-,Products.Description
-,Products.Brand
-,Products.Domain
-,Products.Image
-,Products.Target) ORDER BY RAND() LIMIT 2) As Products
- FROM `core-ssp.machinelearning.usa_products_unnested`  Q 
- INNER JOIN  R ON R.RequestId =  Q.RequestId
-INNER JOIN `core-ssp.machinelearning.GPT_Labels` L ON L.ID=  R.ID
- GROUP BY RequestId, LabelId
+  ,LabelId
+  ,MAX(Date)  AS DATE
+  ,"en-US" as Locale
+  ,MAX(Label) as Label
+  ,ARRAY_AGG( STRUCT (
+  Products.Label
+  ,Products.Title
+  ,Products.Description
+  ,Products.Brand
+  ,Products.Domain
+  ,Products.Image
+  ,Products.Target) ) As Products
+ FROM R
+
+ GROUP BY  LabelId
